c13d3dd91a06de10266cf1977c68787707bb59bd
Le Vuong Nguyen <vuong.se@gmail.com>
2024-11-17 23:06:01 -0500

    Update project structure

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## Intro
+
+This repo is a tool for collecting RSS links from news sites.
+
+## RSS Links
+
+- https://vnexpress.net/rss
+
---
+++ app.py
@@ -0,0 +1,15 @@
+
+
+import feedparser
+
+
+def read_feed(url):
+    feed = feedparser.parse(url)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
